e1a0807cdd95002c615024e0af1c0377f4323464
parham <parham@datastack.academy>
2024-09-30 13:08:51 -0400

    fixed import issues. perspective 2024 nb runs!

---
+++ csp_nodes.py
@@ -0,0 +1,117 @@
+import csp
+from csp import ts
+from datetime import timedelta
+from superstore import (
+    machines,
+    MACHINE_SCHEMA,
+    usage,
+    USAGE_SCHEMA,
+    status,
+    STATUS_SCHEMA,
+    jobs,
+    JOBS_SCHEMA,
+)
+# from perspective import PerspectiveWidget, Table as PerspectiveTable
+from perspective.widget import PerspectiveWidget
+from perspective import table as PerspectiveTable
+from random import random
+from typing import List
+
+
+__all__ = (
+    "MACHINE_SCHEMA",
+    "machines",
+    "USAGE_SCHEMA",
+    "usage",
+    "STATUS_SCHEMA",
+    "status",
+    "JOBS_SCHEMA",
+    "jobs",
+    "machine_usage",
+    "machine_status",
+    "machine_jobs",
+    "push_to_perspective",
+    "push_to_perspective_table",
+)
+
+MACHINE_SCHEMA = dict(MACHINE_SCHEMA)
+USAGE_SCHEMA = dict(USAGE_SCHEMA)
+STATUS_SCHEMA = dict(STATUS_SCHEMA)
+JOBS_SCHEMA = dict(JOBS_SCHEMA)
+
+
+@csp.node
+def machine_usage(
+    machines: List[dict], interval: timedelta = timedelta(seconds=5)
+) -> ts[List[dict]]:
+    with csp.alarms():
+        a_tick = csp.alarm(bool)
+
+    with csp.state():
+        s_machines = machines.copy()
+        s_machine_usage = {}
+
+    with csp.start():
+        csp.schedule_alarm(a_tick, timedelta(), True)
+
+    if csp.ticked(a_tick):
+        for machine in s_machines:
+            machine_id = machine["machine_id"]
+            if machine_id not in s_machine_usage:
+                s_machine_usage[machine_id] = machine.copy()
+            s_machine_usage[machine_id].update(usage(s_machine_usage[machine_id]))
+        csp.output(s_machine_usage.values())
+        csp.schedule_alarm(a_tick, interval, True)
+
+
+@csp.node
+def machine_status(
+    usage: ts[List[dict]], interval: timedelta = timedelta(seconds=5)
+) -> ts[List[dict]]:
+    with csp.alarms():
+        a_tick = csp.alarm(bool)
+
+    with csp.state():
+        csp.make_passive(usage)
+
+    with csp.start():
+        csp.schedule_alarm(a_tick, timedelta(), True)
+
+    if csp.ticked(a_tick) and csp.valid(usage):
+        ret = []
+        for machine in usage:
+            ret.append(status(machine, json=True))
+        csp.output(ret)
+        csp.schedule_alarm(a_tick, interval, True)
+
+
+@csp.node
+def machine_jobs(
+    machines: List[dict], interval: timedelta = timedelta(seconds=5)
+) -> ts[List[dict]]:
+    with csp.alarms():
+        a_tick = csp.alarm(bool)
+
+    with csp.start():
+        csp.schedule_alarm(a_tick, timedelta(), True)
+
+    if csp.ticked(a_tick):
+        ret = []
+        for machine in machines:
+            job = jobs(machine, json=True)
+            if job:
+                ret.append(job)
+        csp.output(ret)
+        csp.schedule_alarm(a_tick, interval, True)
+
+
+@csp.node
+def push_to_perspective(data: ts[list], widget: PerspectiveWidget):
+    if csp.ticked(data):
+        widget.update(data)
+
+
+@csp.node
+def push_to_perspective_table(data: ts[list], table: PerspectiveTable):
+    if csp.ticked(data):
+        table.update(data)
